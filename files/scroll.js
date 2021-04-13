ebb3e3f1f62cce7ef2119cdf5f74ef1b5ae7952d
cerico <garethrobertlee@gmail.com>
2021-04-13 10:30:29 +0100

    q

--- files/scroll.js
+++ files/scroll.js
@@ -1,4 +1,4 @@
-const images = [
+const pimages = [
     { pic: 'd4.jpg.webp', label: 'graphic' },
     { pic: 'atom.jpg.webp', label: 'graphic' },
     { pic: 'pyati.jpg.webp', label: 'graphic' },
@@ -12,14 +12,15 @@ const images = [
     { pic: 'd.jpeg.webp', label: 'graphic' }
     ]
     
-    const handleScroll = () => {
+    const handleScrollu = () => {
+        console.log(155)
         const sectionTops = [].map.call(document.getElementsByClassName('yola'), node => node.getBoundingClientRect().top + window.scrollY)
         const windowPosition = window.scrollY - (window.innerHeight * 0.3)
         const above = sectionTops.filter(e => e < windowPosition).length
-        const image = images[above]
+        const image = pimages[above]
         if (document.getElementById('photo').src !== `files/${image.pic}`) {
             document.getElementById('photo').src = `files/${image.pic}`
             document.getElementById('photo').alt = `files/${image.label}`
         }
       };
-      document.addEventListener('scroll', handleScroll);
+      document.addEventListener('scroll', handleScrollu);
